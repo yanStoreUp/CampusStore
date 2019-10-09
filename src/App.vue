15d7ce741ccd246bf183c503314618ed74389e45
WangYan <wangyan3842@qq.com>
2019-10-09 10:21:28 +0800

    w第一次提交

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,20 @@
     <!-- 使用 router-link 组件来导航. -->
     <!-- 通过传入 `to` 属性指定链接. -->
     <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
-    <router-link to="/foo">Go to Foo</router-link>
-    <router-link to="/bar">Go to Bar</router-link>
+    <router-link to="/register"><mt-button type="primary" class="logon">登录</mt-button></router-link>
+    <router-link to="/enroll"><mt-button type="danger" class="logon">注册</mt-button></router-link>
 
     <!-- 路由出口 -->
     <!-- 路由匹配到的组件将渲染在这里 -->
     <router-view></router-view>
 
-
-    <router-view/>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  
+.logon {
+    width: 80%;
+    margin:10px;
 }
 
 </style>
